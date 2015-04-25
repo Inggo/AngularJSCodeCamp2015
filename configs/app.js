4b30dd16c48a0b87dc390945bf2c6370c64623b5
Inggo <inggo.espinosa@gmail.com>
2015-04-25 14:58:44 +0800

    Added views

--- configs/app.js
+++ configs/app.js
@@ -1,1 +1,16 @@
 var AngularJSApp = angular.module("AngularJSApp", ["ngRoute"]);
+
+AngularJSApp.config(function($routeProvider){
+    $routeProvider
+        .when('/', {
+            controller: 'SearchController',
+            templateUrl: 'views/home.html'
+        })
+        .when('/about/:platformName', {
+            controller: 'SearchController',
+            templateUrl: 'views/about.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
--- controllers/search.js
+++ controllers/search.js
@@ -1,7 +1,20 @@
-AngularJSApp.controller("SearchController", function($scope, $http) {
-    
+AngularJSApp.controller("SearchController", function($scope, $http, $routeParams) {
+
+    $scope.platforms = [];
+    $scope.platform = null;
+
+    $scope.setPlatform = function() {
+        if ($routeParams.platformName) {
+            for (var i = 0; i < $scope.platforms.length; i++) {
+                if ($scope.platforms[i].name.toLowerCase() === $routeParams.platformName)
+                    return $scope.platforms[i];
+            }
+        }
+    };
+
     $scope.onHttpRequestSuccess = function(data) {
         $scope.platforms = data;
+        $scope.platform = $scope.setPlatform();
     };
 
     $http({
