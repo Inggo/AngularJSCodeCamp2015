e1ac6d19b989da727852c6563d6191e2647cd6ec
Inggo <inggo.espinosa@gmail.com>
2015-04-25 13:04:03 +0800

    Added thumbnail directive

---
+++ directives/scripts/thumbnail.js
@@ -0,0 +1,12 @@
+AngularJSApp.directive("thumbnail", function(){
+    return {
+        restrict: 'E',
+        scope: {
+            platform : '='
+        },
+        templateUrl: 'directives/templates/thumbnail.html',
+        link: function($scope, $element, $attribute) {
+            
+        }
+    };
+});
